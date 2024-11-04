8f535f81577cca9df2d292b58551e5e4a1875162
Ber0v <AlexBerov123@gmail.com>
2024-11-04 14:16:16 +0200

    start of the project

---
+++ JamSpot/JamSpotApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JamSpotApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
