49b2ede87d7045e55367273b35b233524f99fe55
Ber0v <AlexBerov123@gmail.com>
2024-11-04 14:31:11 +0200

    starting the base

---
+++ JamSpot/JamSpotApp/Data/JamSpotDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JamSpotApp.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JamSpotApp.Data
+{
+    public class JamSpotDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public JamSpotDbContext(DbContextOptions<JamSpotDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ JamSpot/JamSpotApp/Data/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JamSpotApp.Data.Models
+{
+    public class User : IdentityUser<Guid>
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
