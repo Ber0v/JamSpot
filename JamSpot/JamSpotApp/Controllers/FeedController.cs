692a1a1017aff5b8875c0561517581f953ad9c46
Ber0v <AlexBerov123@gmail.com>
2024-11-05 18:38:47 +0200

    format FeedControler

---
+++ JamSpot/JamSpotApp/Controllers/FeedController.cs
@@ -0,0 +1,35 @@
+﻿using JamSpotApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtJamWebApp.Controllers
+{
+    public class FeedController : Controller
+    {
+        private readonly JamSpotDbContext _context;
+
+        public FeedController(JamSpotDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Feed/Index - Display all posts (Records, Events, MusicianSearch posts)
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        // GET: /Feed/CreatePost - Display form for creating a musician search post
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+
+        // POST: /Feed/CreatePost - Handle form submission for musician search posts
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePost()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
