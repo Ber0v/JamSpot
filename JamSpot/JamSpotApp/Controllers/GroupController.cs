d0257f990699cd74ac73d8e3a4f94f122d4d75e7
Ber0v <AlexBerov123@gmail.com>
2024-11-07 12:08:03 +0200

    Starting with GroupController

---
+++ JamSpot/JamSpotApp/Controllers/GroupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JamSpotApp.Controllers
+{
+    public class GroupController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
---
+++ JamSpot/JamSpotApp/Models/Group/GroupViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JamSpotApp.Models.Group
+{
+    public class GroupViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public required string GroupName { get; set; }
+        public string? Logo { get; set; }
+        public string? Genre { get; set; }
+    }
+}
