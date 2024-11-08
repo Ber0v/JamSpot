e7b8493514ab8ccb65e70fb502ba82526334f127
Ber0v <AlexBerov123@gmail.com>
2024-11-08 10:02:13 +0200

    make UserViewModel

---
+++ JamSpot/JamSpotApp/Models/User/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JamSpotApp.Models.User
+{
+    public class UserViewModel
+    {
+        public required string ProfilePicture { get; set; }
+        public required string UserName { get; set; }
+        public string? UserBio { get; set; }
+        public string? Instrument { get; set; }
+    }
+}
