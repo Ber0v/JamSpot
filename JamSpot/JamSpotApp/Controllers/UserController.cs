3c41612068f75aaa3d0846cf9b6117cdff939736
Ber0v <AlexBerov123@gmail.com>
2024-11-08 18:03:23 +0200

    make edit in usercontroller

--- JamSpot/JamSpotApp/Controllers/UserController.cs
+++ JamSpot/JamSpotApp/Controllers/UserController.cs
@@ -6,6 +6,8 @@ namespace JamSpotApp.Controllers
 {
     using JamSpotApp.Models.User;
     using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+
     public class UserController : Controller
     {
         private readonly JamSpotDbContext context;
@@ -30,6 +32,97 @@ namespace JamSpotApp.Controllers
             };
 
             return View(model);
+        }
+
+        // GET: /User/Edit/{id} - Извежда форма за редактиране на съществуваща user
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var user = await context.Users
+                .Where(g => g.Id == id)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+
+            var model = new EditUserViewModel()
+            {
+                ExistingPicturePath = user.ProfilePicture,
+                UserName = user.UserName,
+                UserBio = user.UserBio,
+                Instrument = user.Instrument
+            };
+
+            return View(model);
+        }
+
+        // POST: /User/Edit/{id} - Обработва редакцията на user
+        [HttpPost]
+        public async Task<IActionResult> Edit(Guid id, EditUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await context.Users.FindAsync(id);
+
+            user.UserName = model.UserName;
+            user.UserBio = model.UserBio;
+            user.Instrument = model.Instrument;
+
+            // Проверка дали има качено ново лого
+            if (model.ProfilePicture != null)
+            {
+                // Изтриваме старото лого от файловата система, ако съществува
+                if (!string.IsNullOrEmpty(user.ProfilePicture) && user.ProfilePicture != DefaultLogo())
+                {
+                    var oldLogoPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", user.ProfilePicture.TrimStart('/'));
+                    if (System.IO.File.Exists(oldLogoPath))
+                    {
+                        System.IO.File.Delete(oldLogoPath);
+                    }
+                }
+
+                // Качваме новото лого и го записваме
+                user.ProfilePicture = UploadLogo(model.ProfilePicture);
+            }
+
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(All));
+        }
+
+        private string UploadLogo(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return null;
+
+            // Генерираме уникално име за файла
+            var uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+
+            // Определяме директорията, където ще съхраняваме файловете
+            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads");
+
+            // Създаваме директорията, ако не съществува
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+
+            // Пълния път на файла
+            var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+            // Качваме файла в директорията
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            // Връщаме относителния път, за да може да бъде използван в HTML
+            return "/uploads/" + uniqueFileName;
+        }
+
+        private string DefaultLogo()
+        {
+            return "https://static.vecteezy.com/system/resources/thumbnails/046/300/546/small/avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-for-social-media-profiles-icons-screensavers-free-png.png";
         }
     }
 }
---
+++ JamSpot/JamSpotApp/Models/User/EditUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace JamSpotApp.Models.User
+{
+    public class EditUserViewModel
+    {
+        public IFormFile? ProfilePicture { get; set; }
+        public required string UserName { get; set; }
+        public string? UserBio { get; set; }
+        public string? Instrument { get; set; }
+
+        public string? ExistingPicturePath { get; set; }
+
+    }
+}
