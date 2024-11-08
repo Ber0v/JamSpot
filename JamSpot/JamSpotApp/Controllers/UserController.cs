61131a556e8b4ad6c7b26b610644d6fadf927bd6
Ber0v <AlexBerov123@gmail.com>
2024-11-08 09:09:45 +0200

    starting with UserController

---
+++ JamSpot/JamSpotApp/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JamSpotApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
