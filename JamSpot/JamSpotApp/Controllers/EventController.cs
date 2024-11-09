4ea653ae99478aa196e6e7afaa2ece7e3823b762
Ber0v <AlexBerov123@gmail.com>
2024-11-09 15:15:25 +0200

    make All in EventController

--- JamSpot/JamSpotApp/Controllers/EventController.cs
+++ JamSpot/JamSpotApp/Controllers/EventController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JamSpotApp.Data;
+using JamSpotApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JamSpotApp.Controllers
 {
+    using JamSpotApp.Models.Event;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+
     public class EventController : Controller
     {
-        public IActionResult Index()
+        private readonly JamSpotDbContext context;
+        private readonly UserManager<User> _userManager;
+
+        public EventController(JamSpotDbContext _context, UserManager<User> userManager)
         {
-            return View();
+            context = _context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> All()
+        {
+            var model = await context.Events
+                .Include(p => p.Organizer)
+                .Select(p => new EventViewModel()
+                {
+                    EventName = p.EventName,
+                    EventDescription = p.EventDescription,
+                    Organizer = p.Organizer.GroupName,
+                    Location = p.Location,
+                    Date = p.Date.ToString("dd-MM-yy"),
+                })
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(model);
         }
     }
 }
