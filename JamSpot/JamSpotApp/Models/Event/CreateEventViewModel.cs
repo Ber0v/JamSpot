8489967ff8df765af20b0dc1b3ff399fb7cdd137
Ber0v <AlexBerov123@gmail.com>
2024-11-15 21:04:52 +0200

    update models view for event

--- JamSpot/JamSpotApp/Models/Event/CreateEventViewModel.cs
+++ JamSpot/JamSpotApp/Models/Event/CreateEventViewModel.cs
@@ -1,10 +1,23 @@
-﻿namespace JamSpotApp.Models.Event
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JamSpotApp.Models.Event
 {
     public class CreateEventViewModel
     {
+        [StringLength(50, ErrorMessage = "The event name cannot exceed 50 characters."),
+         MinLength(1, ErrorMessage = "The event name must be at least 1 character.")]
         public string EventName { get; set; } = string.Empty;
+
+        [StringLength(300, ErrorMessage = "The event description cannot exceed 300 characters."),
+         MinLength(1, ErrorMessage = "The event description must be at least 1 character.")]
         public string EventDescription { get; set; } = string.Empty;
+
+        [Range(0, 999.00, ErrorMessage = "The price must be a positive value and cannot exceed 999.")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "The location is required.")]
         public string Location { get; set; }
-        public string Date { get; set; } = DateTime.Today.ToString("dd-MM-yy");
+        public string Date { get; set; } = DateTime.Today.ToString("dd.MM.yyyy");
+        public string Hour { get; set; } = TimeOnly.FromDateTime(DateTime.Now).ToString("HH\\:mm");
     }
 }
--- JamSpot/JamSpotApp/Models/Event/EventViewModel.cs
+++ JamSpot/JamSpotApp/Models/Event/EventViewModel.cs
@@ -5,8 +5,10 @@
         public Guid Id { get; set; }
         public required string EventName { get; set; }
         public required string EventDescription { get; set; }
+        public required double Price { get; set; }
         public required string Location { get; set; }
         public required string Date { get; set; }
+        public required string Hour { get; set; }
         public required string Organizer { get; set; }
 
     }
