a6510ef2486ee4e0eb5c2a6f474caacca1a43b36
Ber0v <AlexBerov123@gmail.com>
2024-11-15 11:01:53 +0200

    Adding Migration

---
+++ JamSpot/JamSpotApp/Migrations/20241115085806_UpdateEvent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JamSpotApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "Hour",
+                table: "Events",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Events",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hour",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Events");
+        }
+    }
+}
