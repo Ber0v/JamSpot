239ca7cbcb27acc7ecdf821c0743187f7c86de3b
Ber0v <AlexBerov123@gmail.com>
2024-11-15 11:01:32 +0200

    update event database

--- JamSpot/JamSpotApp/Data/Models/Event.cs
+++ JamSpot/JamSpotApp/Data/Models/Event.cs
@@ -5,9 +5,10 @@
         public Guid Id { get; set; } = Guid.NewGuid();
         public required string EventName { get; set; }
         public required string EventDescription { get; set; }
+        public double Price { get; set; }
         public required string Location { get; set; }
         public DateTime Date { get; set; }
-
+        public TimeOnly Hour { get; set; }
         public Guid OrganizerId { get; set; }
         public required User Organizer { get; set; }
 
