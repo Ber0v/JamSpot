2144c13ed1acb7bce579a5b9b67fe136c3635d6a
Ber0v <AlexBerov123@gmail.com>
2024-11-19 17:38:56 +0200

    starting whit MusicController

---
+++ JamSpot/JamSpotApp/Controllers/MusicController.cs
@@ -0,0 +1,26 @@
+﻿using JamSpotApp.Data;
+using JamSpotApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JamSpotApp.Controllers
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public class MusicController : Controller
+    {
+        private readonly JamSpotDbContext context;
+        private readonly UserManager<User> _userManager;
+
+        public MusicController(JamSpotDbContext _context, UserManager<User> userManager)
+        {
+            context = _context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            return View();
+        }
+    }
+}
