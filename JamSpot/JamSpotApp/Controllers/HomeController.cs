230606d1f53d8fe0981917c49edffe0a802593d2
Ber0v <AlexBerov123@gmail.com>
2024-11-20 23:23:02 +0200

    make Search option in HomeController

--- JamSpot/JamSpotApp/Controllers/HomeController.cs
+++ JamSpot/JamSpotApp/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
+using JamSpotApp.Data;
 using JamSpotApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace JamSpotApp.Controllers
@@ -7,10 +9,12 @@ namespace JamSpotApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly JamSpotDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, JamSpotDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -18,10 +22,27 @@ namespace JamSpotApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public async Task<IActionResult> AutoComplete(string query)
         {
-            return View();
+            if (string.IsNullOrEmpty(query))
+            {
+                return Json(new { users = new List<object>(), groups = new List<object>() });
+            }
+
+            var users = await _context.Users
+                .Where(u => EF.Functions.Like(u.UserName, $"%{query}%"))
+                .Select(u => new { u.Id, u.UserName })
+                .ToListAsync();
+
+            var groups = await _context.Groups
+                .Where(g => EF.Functions.Like(g.GroupName, $"%{query}%"))
+                .Select(g => new { g.Id, g.GroupName })
+                .ToListAsync();
+
+            return Json(new { users = users, groups = groups });
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
