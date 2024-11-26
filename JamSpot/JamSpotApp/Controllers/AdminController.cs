f1db3d4dde98801a473d64a666e3259f4d29b736
Ber0v <AlexBerov123@gmail.com>
2024-11-26 21:32:22 +0200

    finally, after a thousand torments, I made the transfer to adminmode

---
+++ JamSpot/JamSpotApp/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JamSpotApp.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        //
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Задължителен за защита от CSRF
+        public IActionResult ToggleAdminMode(bool isAdminModeEnabled)
+        {
+            if (isAdminModeEnabled)
+            {
+                Response.Cookies.Append("IsAdminMode", "true", new CookieOptions { HttpOnly = true, Expires = DateTimeOffset.Now.AddHours(1) });
+            }
+            else
+            {
+                Response.Cookies.Delete("IsAdminMode");
+            }
+
+            return Redirect(Request.Headers["Referer"].ToString() ?? "/");
+        }
+    }
+}
