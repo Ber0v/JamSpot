e6c2f008a5a0106f240e52f1c0184a367c3346b4
Ber0v <AlexBerov123@gmail.com>
2024-11-26 23:45:40 +0200

    preparing the rest of the admin part

---
+++ JamSpot/JamSpotApp/Areas/Admin/Pages/Feed/AdminFeed.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JamSpotApp.Areas.Admin.Pages.Feed
+{
+    public class FeedEventsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ JamSpot/JamSpotApp/Areas/Admin/Pages/Groups/AdminGroups.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JamSpotApp.Areas.Admin.Pages.Groups
+{
+    public class AdminGroupsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
